a19988ba5aba47979a2b237e6861d400d472c6cd
Joao Pedro <jpc5626@gmail.com>
2020-07-13 13:13:33 -0300

    ajusts

--- pages/index.js
+++ pages/index.js
@@ -11,13 +11,13 @@ const Index = () => {
   return (
     <div>
       <PageTitle title='Seja Bem-Vindo' />
-      <p className='mt-12 text-center'>
+      <p className='mt-12 mb-20 text-center'>
         O Estabelecimento X sempre busca atender melho os seus clientes.<br />
         Por Isso, estamos sempre abertos a sua opiniaão.
       </p>
       <div className='text-center mt-12 mb-12'>
         <Link href='/pesquisa'>
-          <a className='bg-gray-700 px-12 py-4  rounded-lg shadow-lg hover:shadow text-white'>Dar opiniaão ou sugestão</a>
+          <a className='bg-gray-700 mb-20 px-12 py-4  rounded-lg shadow-lg hover:shadow text-white'>Dar opiniaão ou sugestão</a>
         </Link>
       </div>
       {!data && <p>Carregando...</p>}
